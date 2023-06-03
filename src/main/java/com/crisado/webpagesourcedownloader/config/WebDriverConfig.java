5e38d7289892c076386ae04763b402e6a3bda12c
crisadoicinf <cristian.icinf@gmail.com>
2023-06-03 21:55:41 +0100

    add remote driver

--- src/main/java/com/crisado/webpagesourcedownloader/config/WebDriverConfig.java
+++ src/main/java/com/crisado/webpagesourcedownloader/config/WebDriverConfig.java
@@ -1,16 +1,28 @@
 package com.crisado.webpagesourcedownloader.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class WebDriverConfig {
 
-    public ChromeDriver getChromeDriver() {
+    public WebDriver getChromeDriver() {
         WebDriverManager.chromedriver().setup();
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new", "--no-sandbox");
         return new ChromeDriver(options);
+    }
+
+    public WebDriver getRemoteChromeDriver() throws URISyntaxException, MalformedURLException {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new", "--no-sandbox");
+        return new RemoteWebDriver(new URI("http://localhost:4444").toURL(), options);
     }
 }
